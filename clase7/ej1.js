320a9cc1e62dec8ea3efd5ffedc005b693a46c0e
Matias Sosa <matiassosas@gmail.com>
2017-08-30 18:38:17 -0300

    clase 7

---
+++ clase7/ej1.js
@@ -0,0 +1,14 @@
+var edad = prompt("ingrese su edad");
+
+
+
+
+console.log("mi edad es: " + edad);
+
+alert("CUIDADO!!")
+//console.log(edad);
+
+
+/*   
+
+*/
---
+++ clase7/ej3.js
@@ -0,0 +1,10 @@
+var edad = 20;
+
+edad = edad * 365;
+
+if (edad > 11000) {
+	console.log(edad);
+}
+else {
+	console.log("tu edad es menor a 11000 dias");
+}
